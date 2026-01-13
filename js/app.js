43ef7a2d5145f1c47d8cb6b5c74db76ae3f43621
sharongrace06 <167194560+sharongrace06@users.noreply.github.com>
2026-01-13 18:10:22 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 console.log("App Loaded")
+
+// Select DOM elements
+const form = document.querySelector("form");
+const placeInput = document.getElementById("placeName");
+const latitudeInput = document.getElementById("latitude");
+const longitudeInput = document.getElementById("longitude");
+const errorInput = document.getElementById("errorMessage");
+
+
