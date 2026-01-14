4d59f5d343098f4c758ccfeb316d248826597340
sharongrace06 <167194560+sharongrace06@users.noreply.github.com>
2026-01-14 16:16:30 +0530

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,1 +1,28 @@
 console.log("map.js loaded");
+
+let map;
+function initMap(){
+  // 1. Create map instance
+  const map = L.map("map");
+  
+// 2. Set initial view (lat, lng, zoom)
+  //map.setView([latitude, longitude], 5);
+
+  // 3. Attach a tile layer
+  //L.tileLayer()
+
+  
+};
+
+function addMarker(location){};
+
+function renderMarkers(locations){};
+
+
+
+
+window.mapRenderer = {
+  initMap: initMap,
+  addMarker: addMarker,
+  renderMarkers: renderMarkers
+};
