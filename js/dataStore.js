e4f39cb27a5ec6dedf9d574aafcbfdd1d3bed231
sharongrace06 <167194560+sharongrace06@users.noreply.github.com>
2026-01-15 12:54:04 +0530

    Update dataStore.js
    added the loadFormStorage function

--- js/dataStore.js
+++ js/dataStore.js
@@ -13,8 +13,23 @@ function getLocations(){
   return locations;
 };
 
+//Load saved locations into memory if they exist
+function loadFromStorage(){         
+  const storedData = localStorage.getItem("locations");  //localStorage → browser storage
+
+  if(storedData){
+    locations = JSON.parse(storedData);      // if there is data then it will convert text back to real data (storedData is text)
+  };                                         //JSON.parse() converts it into array 
+
+
+  
+};                              
+
+
+
 window.dataStore = {
   addLocation : addLocation,
   getLocations : getLocations
 };
+
 
