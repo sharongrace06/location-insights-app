c9e09c625c21db541b3246b56184f2f5791db804
sharongrace06 <167194560+sharongrace06@users.noreply.github.com>
2026-01-16 15:50:28 +0530

    Update export.js
    added the download map function

--- js/export.js
+++ js/export.js
@@ -3,4 +3,18 @@
 function exportMap(){
   console.log("Export map triggered");
   
+  const mapElement = document.getElementById("map");
+  
+  html2canvas(mapElement).then((canvas) => {
+    console.log("Canvas created:", canvas);
+    
+    const imageData = canvas.toDataURL("image/png");
+    const  anchor = document.createElement("a");
+    
+    anchor.setAttribute("href",imageData);
+    anchor.setAttribute("download","map.png");
+    anchor.click();
+    
+  });
 };
+
