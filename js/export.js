070cc781dcd0b5eae7429c6c8ca72b3e80650b09
sharongrace06 <167194560+sharongrace06@users.noreply.github.com>
2026-01-17 20:39:44 +0530

    Update export.js added the export function for map2

--- js/export.js
+++ js/export.js
@@ -18,4 +18,22 @@ function exportMap(){
   });
 };
 
+function exportInsightsMap(){
+  const map2Element = document.getElementById("insights-map");
+  
+  html2canvas(map2Element, {useCORS: true, allowTaint: true}).then((canvas) => {
+    console.log("Canvas created:", canvas);
+
+    const image2Data = canvas.toDataURL("image/png");
+    const  anchor = document.createElement("a");
+    
+    anchor.setAttribute("href",image2Data);
+    anchor.setAttribute("download","map2.png");
+    anchor.click();
+  });
+
+  
+};
+
+
 
