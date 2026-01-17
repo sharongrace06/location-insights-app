39adecec21e1ed0a8f3550d1ccb2aa15292c5b25
sharongrace06 <167194560+sharongrace06@users.noreply.github.com>
2026-01-17 16:34:26 +0530

    Update insights.js
    insights functions added

--- js/insights.js
+++ js/insights.js
@@ -1,2 +1,61 @@
 console.log("insights.js loaded");
 
+//If I give this function data, it gives me a single number back.
+function getTotalLocations(locations){
+  return locations.length ; 
+  
+};
+
+//This function converts raw location objects into a drawable path.
+function getSequentialPath(locations) {
+  const copy =  [...locations]; // create a copy of the locations array
+  copy.sort((a,b) => {
+    const timeA = new Date(a.timestamp).getTime();
+    const timeB = new Date(b.timestamp).getTime();
+    if (timeA < timeB) return -1;
+    if (timeA > timeB) return 1;
+    return 0;
+     });
+
+  const locationPath = copy.map(item => {
+    return [item.latitude, item.longitude];
+});
+  
+};
+
+
+
+//Every location becomes a heat ‘dot’ with equal weight.
+function getHeatPoints(locations) {
+  const heatPoints = locations.map(location => {
+  
+    const latitude = location.latitude;
+    const longitude = location.longitude;
+    const intensity = 1;
+    return [latitude, longitude, intensity]  ;
+    });
+
+  return heatPoints;
+};
+
+
+
+
+
+window.insights = {
+  getTotalLocations: getTotalLocations,
+  getSequentialPath: getSequentialPath,
+  getHeatPoints: getHeatPoints
+  
+};
+
+console.log(
+  window.insights.getHeatPoints(dataStore.getLocations()));
+
+console.log(
+  window.insights.getSequentialPath(dataStore.getLocations())
+);
+
+
+
+
